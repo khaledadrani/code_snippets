e2738607a75bd41fff2d78a892a7f10810877171
khaled-adrani <khaledadrani@gmail.com>
2021-11-22 16:33:44 +0100

    init react and mongodb

---
+++ mongodb/mongodb_basics.js
@@ -0,0 +1,13 @@
+// show dbs
+
+// use sample_training
+
+// show collections
+
+db.zips.find({"state": "NY"})
+
+db.zips.find({"state": "NY"}).count()
+
+db.zips.find({"state": "NY", "city": "ALBANY"})
+
+db.zips.find({"state": "NY", "city": "ALBANY"}).pretty()
